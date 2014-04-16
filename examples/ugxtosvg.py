fb7eeb4807aff13e7bd582424ac45ac45b8e7435
stephanmg <stephan@syntaktischer-zucker.de>
2014-04-16 10:29:11 +0200

    Forked ndebeiss svg3d repository. Adding ugx to svg exporter (Python).

---
+++ examples/ugxtosvg.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import xml.etree.ElementTree as ET
+
+def ugtosvg(fin, fout):
+   prepend = """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11-flat-20030114.dtd">
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+xmlns:z="http://debeissat.nicolas.free.fr/svg3d/svg3d.rng" width="120%" height="120%" onload="init(this)" z:sortAlgo="allToAll">
+    <script type="text/ecmascript" xlink:href="../svg3d/svg3d.js" />
+    <script type="text/ecmascript" xlink:href="../svg3d/svg3d_parsing.js" />
+    <script type="text/ecmascript" xlink:href="../svg3d/dom_utils.js" />
+   <title>SVG animation - auto-generated (no title set!)</title>
+"""
+   middle = """
+"""
+   append = """<\svg>
+"""
+
+   tree = ET.parse(fin)
+   root = tree.getroot()
+   digits = []
+   quads = []
+   for atype in tree.findall('vertices'):
+      digits = atype.text.split(" ")
+   
+   vertices = []
+   for vertex in range(0, len(digits)/3, 1):
+      vertices.append([float(digits[vertex]), float(digits[vertex+1]), float(digits[vertex+2])])
+   
+   for atype in tree.findall('quadrilaterals'):
+      digits = atype.text.split(" ")
+   
+   for quad in range(0, len(digits)/4, 1):
+      quads.append([int(digits[quad]), int(digits[quad+1]), int(digits[quad+2]), int(digits[quad+3])])
+   
+   counter = 1
+   for quad in quads:
+      quadstr = "M"
+      for q in quad:
+         for coord in range(0, len(vertices[q])-1, 1):
+            quadstr += str(vertices[q][coord]) + ","
+         
+         quadstr += str(vertices[q][len(vertices[q])-1]) + "L"
+      
+
+      final_str = ""
+      final_str += '<path id="'
+      final_str += str(counter)
+      final_str += '" style="fill: red;" d="';
+      final_str += quadstr[:-1] + 'z"' 
+      final_str += ' z:threeD="true">'
+      final_str += "\n"
+      final_str += '<z:rotation incRotX="0.07" incRotY="0.03" incRotZ="0.02"/>'
+      final_str += "\n"
+      final_str += '<\path>'
+      final_str += "\n"
+      counter = counter + 1
+      middle += final_str
+   
+   prepend += middle
+   prepend += append
+   print(prepend)
+   
+if __name__ == "__main__":
+   fin  = "unit_cube_test.ugx"
+   fout = "unit_cube_test.svg"
+   ugtosvg(fin, fout)
