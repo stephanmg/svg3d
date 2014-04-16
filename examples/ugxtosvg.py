9328b60f8faf4d7f6cd58982e187627b18035a64
stephanmg <stephan@syntaktischer-zucker.de>
2014-04-16 13:50:03 +0200

    Generalized code.

--- examples/ugxtosvg.py
+++ examples/ugxtosvg.py
@@ -2,19 +2,27 @@
 # -*- coding: utf-8 -*-
 # author: stephan
 
-# imports
+# import xml parsing utility
 import xml.etree.ElementTree as ET
 
-# functions
+# wrappers for exporters
+def ugtosvg1(fin, fout, dim):
+   """ Exports a 1d grid to a svg in 3d """
+   ugtosvg(fin, fout, dim)
 
-def ugtosvg2(fin, fout):
-   """ Exports an surface grid in 2d to a svg in 2d """
-   pass
+def ugtosvg2(fin, fout, dim):
+   """ Exports an surface grid in 2d to a svg in 3d """
+   ugtosvg(fin, fout, dim)
 
-def ugtosvg3(fin, fout):
+def ugtosvg3(fin, fout, dim):
    """ Export an volume grid in 3d to a svg in 3d """
+   ugtosvg(fin, fout, dim)
+
+# exporter
+def ugtosvg(fin, fout, dim_=-1):
+   """ Export a grid either 1d, 2d, or 3d to a svg in 3d """
    # debug
-   debug = False
+   debug = True
 
    # string constants
    prepend = """<?xml version="1.0" encoding="UTF-8"?>
@@ -44,9 +52,28 @@ def ugtosvg3(fin, fout):
    # setup vertices
    for atype in tree.findall('vertices'):
       digits = atype.text.split(" ")
-   
-   for vertex in range(0, len(digits)-2, 3):
-      vertices.append([float(digits[vertex]), float(digits[vertex+1]), float(digits[vertex+2])])
+
+   if ( dim_ == -1 ):
+      dim = int(tree.findall('vertices')[0].get('coords'))
+   else:
+      dim = dim_
+
+   if (not (dim >= 1 or dim <= 3)):
+      print("Dim is enforced to be in the range [1, 3]. Aborting.")
+      return
+
+   # consistency check
+   if (dim == 3):
+     for vertex in range(0, len(digits)-2, dim):
+         vertices.append([float(digits[vertex]), float(digits[vertex+1]), float(digits[vertex+2])])
+   elif (dim == 2):
+     for vertex in range(0, len(digits)-2, dim):
+         vertices.append([float(digits[vertex]), float(digits[vertex+1]), 0.0])
+   elif (dim == 1):
+     for vertex in range(0, len(digits)-2, dim):
+         vertices.append([float(digits[vertex]), 0.0, 0.0])
+   else:
+      pass
    
    # process quadrilaterals
    for atype in tree.findall('quadrilaterals'):
