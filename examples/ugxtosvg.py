a0f281d92f767795d650162a17feeacc034dc869
stephanmg <stephan@syntaktischer-zucker.de>
2014-04-17 09:26:53 +0200

    Handling 1d, 2d, 3d manifolds correctly.

--- examples/ugxtosvg.py
+++ examples/ugxtosvg.py
@@ -8,17 +8,17 @@
 import xml.etree.ElementTree as ET
 
 # wrappers for exporters
-def ugtosvg1(fin, fout, dim):
+def ugtosvg1(fin, fout):
    """ Exports a 1d grid to a svg in 3d """
-   ugtosvg(fin, fout, dim)
+   return ugtosvg(fin, fout, 1)
 
-def ugtosvg2(fin, fout, dim):
+def ugtosvg2(fin, fout):
    """ Exports an surface grid in 2d to a svg in 3d """
-   ugtosvg(fin, fout, dim)
+   ugtosvg(fin, fout, 2)
 
-def ugtosvg3(fin, fout, dim):
+def ugtosvg3(fin, fout):
    """ Export an volume grid in 3d to a svg in 3d """
-   ugtosvg(fin, fout, dim)
+   ugtosvg(fin, fout, 3)
 
 # exporter
 def ugtosvg(fin, fout, dim_=-1):
@@ -28,17 +28,19 @@ def ugtosvg(fin, fout, dim_=-1):
 
    # string constants
    prepend = """<?xml version="1.0" encoding="UTF-8"?>
-   <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11-flat-20030114.dtd">
-   <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-   xmlns:z="http://debeissat.nicolas.free.fr/svg3d/svg3d.rng" width="120%" height="120%" onload="init(this)" z:sortAlgo="allToAll">
-       <script type="text/ecmascript" xlink:href="../svg3d/svg3d.js" />
-       <script type="text/ecmascript" xlink:href="../svg3d/svg3d_parsing.js" />
-       <script type="text/ecmascript" xlink:href="../svg3d/dom_utils.js" />
-      <title>SVG animation - auto-generated (no title set!)</title>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11-flat-20030114.dtd">
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+xmlns:z="http://debeissat.nicolas.free.fr/svg3d/svg3d.rng" width="100%" height="100%" onload="svg3d.init(this)" z:sortAlgo="allToAll">
+    <script type="text/ecmascript" xlink:href="../svg3d/svg3d.js" />
+    <script type="text/ecmascript" xlink:href="../svg3d/svg3d_parsing.js" />
+    <script type="text/ecmascript" xlink:href="../svg3d/dom_utils.js" />
+        <title>SVG animation - Ball</title>
+    <g id="g1" onclick="svg3d.toggleRotation()">
    """
    middle = """
    """
-   append = """</svg>
+   append = """</g> 
+</svg>
    """
 
    # read the xml tree
@@ -74,8 +76,6 @@ def ugtosvg(fin, fout, dim_=-1):
    elif (dim == 1):
      for vertex in range(0, len(digits)-2, dim):
          vertices.append([float(digits[vertex]), 0.0, 0.0])
-   else:
-      pass
    
    # process quadrilaterals
    for atype in tree.findall('quadrilaterals'):
@@ -95,7 +95,7 @@ def ugtosvg(fin, fout, dim_=-1):
       final_str = ""
       final_str += '<path id="'
       final_str += str(counter)
-      final_str += '" style="fill: red;" d="';
+      final_str += '" style="fill: red; stroke:black; fill-opacity:0.1" d="';
       final_str += quadstr[:-1] + 'z"' 
       final_str += ' z:threeD="true">'
       final_str += "\n"
@@ -120,10 +120,10 @@ def ugtosvg(fin, fout, dim_=-1):
    for tri in tris:
       tristr = "M"
       for t in tri:
-         for coord in range(0, len(vertices[q])-1, 1):
-            tristr += str(vertices[q][coord]) + ","
+         for coord in range(0, len(vertices[t])-1, 1):
+            tristr += str(vertices[t][coord]) + ","
          
-         tristr += str(vertices[q][len(vertices[q])-1]) + "L"
+         tristr += str(vertices[t][len(vertices[t])-1]) + "L"
       
       final_str = ""
       final_str += '<path id="'
@@ -138,6 +138,40 @@ def ugtosvg(fin, fout, dim_=-1):
       final_str += "\n"
       counter = counter + 1
       middle += final_str
+
+   # process edges
+   digits  = []
+   edges   = []
+   counter = 1
+
+   if (dim == 1):
+      for atype in tree.findall('edges'):
+            digits = atype.text.split(" ")
+         
+      for edge in range(0, len(digits)-2, 2):
+         edges.append([int(digits[edge]), int(digits[edge+1]), int(digits[edge+2])])
+         
+      for edge in edges:
+         edgestr = "M"
+         for e in edge:
+            for coord in range(0, len(vertices[e])-1, 1):
+               edgestr += str(vertices[e][coord]) + ","
+            
+            edgestr += str(vertices[e][len(vertices[e])-1]) + "L"
+         
+         final_str = ""
+         final_str += '<path id="'
+         final_str += str(counter)
+         final_str += '" style="fill: red;" d="';
+         final_str += edgestr[:-1] + 'z"' 
+         final_str += ' z:threeD="true">'
+         final_str += "\n"
+         final_str += '<z:rotation incRotX="0.07" incRotY="0.03" incRotZ="0.02"/>'
+         final_str += "\n"
+         final_str += '</path>'
+         final_str += "\n"
+         counter = counter + 1
+         middle += final_str
 
    prepend += middle
    prepend += append
